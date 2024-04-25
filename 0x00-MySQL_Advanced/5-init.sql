f42decc296eb44bf19a837090e6f21c57b9123ac
Rowida <rowida91200@gmail.com>
2024-04-25 10:37:49 +0200

    Test

--- 0x00-MySQL_Advanced/5-init.sql
+++ 0x00-MySQL_Advanced/5-init.sql
@@ -1,14 +1,12 @@
 -- Initial
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    email varchar(255) not null,
-    name varchar(255),
-    valid_email boolean not null default 0,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
-INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
-INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
+END $$
+DELIMITER ;
